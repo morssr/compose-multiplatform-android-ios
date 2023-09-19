4b3a501be03066dcf65051dece12e546ee1f31ca
Mor Shaar <morsrs1@gmail.com>
2023-09-19 20:24:37 +0300

    general: Animate navigation between screens

--- shared/src/commonMain/kotlin/com/mls/kmp/mor/nytnewskmp/MySharedApp.kt
+++ shared/src/commonMain/kotlin/com/mls/kmp/mor/nytnewskmp/MySharedApp.kt
@@ -3,12 +3,15 @@ package com.mls.kmp.mor.nytnewskmp
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.mls.kmp.mor.nytnewskmp.ui.HomeScreenRoute
 import com.mls.kmp.mor.nytnewskmp.ui.theme.MyAppTheme
 
 @Composable
 fun MyApp(darkTheme: Boolean = isSystemInDarkTheme()) {
     MyAppTheme(darkTheme) {
-        Navigator(HomeScreenRoute())
+        Navigator(HomeScreenRoute()) {
+            SlideTransition(it)
+        }
     }
 }
